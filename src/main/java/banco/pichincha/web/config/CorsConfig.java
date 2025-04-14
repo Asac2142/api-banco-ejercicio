22a5118a60583b447f0f3973b136026bde8b19c5
Andres Alban <bethesdas101@gmail.com>
2025-04-14 12:48:29 -0500

    adding CORS config for frontend communication

---
+++ src/main/java/banco/pichincha/web/config/CorsConfig.java
@@ -0,0 +1,16 @@
+package banco.pichincha.web.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig implements WebMvcConfigurer {
+    @SuppressWarnings("null")
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
